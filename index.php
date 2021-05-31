28bc2a5204e722e7a166a2455e2fe0ae58ca0245
Robert-Stefan <robertboss05@hotmail.it>
2021-05-31 18:09:02 +0200

    Finish

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /**
  * USER CLASS
  */
+
 class User {
     /**
      * Attributes/Properties/Variables
@@ -23,11 +24,12 @@ class User {
         $this->year = $year;
 
     }
+
     /**
      * SET PEGI
      */
     public function setPegi($pegi) {
-        if($pegi > 18) {
+        if($pegi >= 18) {
             echo 'Attenzione! Film per maggiorenni!';
         } else {
             echo 'Il film è adatto a tutta la famiglia!';
@@ -40,17 +42,18 @@ class User {
 $jurassic = new User('Jurassic World', 'Quando i dinosauri seminano il caos sull\'isola sei ragazzi di un campo avventura sul versante opposto di Isla Nublar devono unire le forze per sopravvivere.', 2020);
 
 //PRINT DATI
-echo "<p>$jurassic->title</p>";
+echo "<h2>$jurassic->title</h2>";
 echo "<p>$jurassic->description</p>";
 echo "<p>Film rilasciato il: $jurassic->year</p>";
 $jurassic->setPegi(14);
 
+// ISTANCE 2
+$american_pie = new User('American Pie', 'Finite le superiori un gruppo di amici un po\' imbranati, che hanno tutti problemi a trovare una ragazza, decidono di fare una sfida per la festa di fine perdere la verginità prima di iscriversi al college. Non sarà facile.', 1999); 
 
-
-
-
-
-
-
+// PRINT DATI
+echo "<h2>$american_pie->title</h2>";
+echo "<p>$american_pie->description</p>";
+echo "<p>Film rilasciato il: $american_pie->year</p>";
+$american_pie->setPegi(18);
 
 ?>
